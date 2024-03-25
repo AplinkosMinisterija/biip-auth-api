566c15024ddb0c29966a59067851bcdf95b592df
Pranciskus Ambrazas <p.ambrazas@gmail.com>
2024-03-25 08:25:00 +0200

    fixing seeds - double check before creating

--- services/seed.service.ts
+++ services/seed.service.ts
@@ -11,35 +11,55 @@ import { AppType } from './apps.service';
 export default class SeedService extends moleculer.Service {
   @Action()
   async real(ctx: Context<Record<string, unknown>>) {
-    // create apps
-    await ctx.call('apps.create', {
-      name: 'Admin',
-      type: AppType.ADMIN,
-      url: 'https://admin.biip.lt',
-      settings: {
-        productNameTo: 'BĮIP administravimo sistemos',
-      },
-    });
-    
-    await ctx.call('apps.create', {
-      name: 'Vidiniai naudotojai',
-      type: AppType.USERS,
-      url: 'https://admin.biip.lt',
-      settings: {
-        productNameTo: 'BĮĮP naudotojų valdymo sistemos',
-      },
-    });
+    const adminEmail = process.env.DEFAULT_SUPER_ADMIN_EMAIL || 'superadmin@biip.lt';
+    const adminAppExists = await ctx.call('apps.count', { query: { type: AppType.ADMIN } });
+    const usersAppExists = await ctx.call('apps.count', { query: { type: AppType.USERS } });
+    const userExists = await ctx.call('users.count', { query: { email: adminEmail } });
 
-    // create local user
-    await ctx.call('usersLocal.invite', {
-      email: 'superadmin@am.lt',
-      firstName: 'Super',
-      lastName: 'Admin',
-      password: 'Slaptazodis1@',
-      phone: '+37060000000',
-      type: UserType.SUPER_ADMIN,
-      doNotSendEmail: true,
-    });
+    if (!adminAppExists) {
+      // create admin app
+      await ctx.call('apps.create', {
+        name: 'Admin',
+        type: AppType.ADMIN,
+        url: 'https://admin.biip.lt',
+        settings: {
+          productNameTo: 'BĮIP administravimo sistemos',
+        },
+      });
+    }
+
+    if (!usersAppExists) {
+      // create users app
+      await ctx.call('apps.create', {
+        name: 'Vidiniai naudotojai',
+        type: AppType.USERS,
+        url: 'https://admin.biip.lt',
+        settings: {
+          productNameTo: 'BĮĮP naudotojų valdymo sistemos',
+        },
+      });
+    }
+
+    if (!userExists) {
+      // create local user
+      await ctx.call(
+        'usersLocal.invite',
+        {
+          email: adminEmail,
+          firstName: 'Super',
+          lastName: 'Admin',
+          password: 'Slaptazodis1@',
+          phone: '+37060000000',
+          type: UserType.SUPER_ADMIN,
+          doNotSendEmail: true,
+        },
+        {
+          meta: {
+            hasPermissions: true,
+          },
+        },
+      );
+    }
 
     return true;
   }
@@ -58,8 +78,9 @@ export default class SeedService extends moleculer.Service {
       .waitForServices(['users', 'usersLocal', 'apps', 'groups', 'permissions'])
       .then(async () => {
         const usersCount: number = await this.broker.call('users.count');
+        const appsCount: number = await this.broker.call('apps.count');
 
-        if (usersCount === 0) {
+        if (!usersCount || !appsCount) {
           await this.broker.call('seed.real', {}, { timeout: 120 * 1000 });
         }
       });
